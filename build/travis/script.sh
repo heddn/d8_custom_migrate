7214780b672808ba122df2e2363571a6ced9565b
heddn <lucashedding@1463982.no-reply.drupal.org>
2016-03-30 10:29:17 -0600

    reduce the tests and update badges

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -3,8 +3,9 @@ set -ev
 # Run core's PHPUnit tests cause they are fast.
 export SIMPLETEST_DB="sqlite://tmp/site.sqlite"
 export SIMPLETEST_BASE_URL="http://localhost:8080"
-cd ${TRAVIS_BUILD_DIR}/web
-${TRAVIS_BUILD_DIR}/vendor/bin/phpunit -c core --exclude-group Composer
+# PHP Unit takes too long
+#cd ${TRAVIS_BUILD_DIR}/web
+#${TRAVIS_BUILD_DIR}/vendor/bin/phpunit -c core --exclude-group Composer
 # Run PhantomJS.
 mkdir -p /tmp/pjsdrivercache/phantomjs
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true ${TRAVIS_BUILD_DIR}/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 > /tmp/gastonjs.log &
