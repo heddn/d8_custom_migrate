feec39b89cd2feecd3402c7a6a9a8e2e720c5456
heddn <lucashedding@1463982.no-reply.drupal.org>
2017-04-07 14:32:26 -0600

    add npm/yarn to travis

--- build/travis/install.sh
+++ build/travis/install.sh
@@ -5,4 +5,9 @@ cd ${TRAVIS_BUILD_DIR}/web
 printf "<?php\n\$databases['default']['default'] = ['database' => 'tmp/site.sqlite', 'prefix' => '', 'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite', 'driver' => 'sqlite'];" > ${TRAVIS_BUILD_DIR}/web/sites/default/settings.local.php
 ${TRAVIS_BUILD_DIR}/vendor/bin/drush site-install --verbose --yes config_installer --db-url=sqlite://tmp/site.sqlite
 ${TRAVIS_BUILD_DIR}/vendor/bin/drush runserver --dns localhost:8080 - &
+nvm install 4
+npm install -g yarn
+cd ${TRAVIS_BUILD_DIR}/web/themes/custom/custom_theme
+yarn install
+yarn run gulp
 sleep 3
